697899014801b2bcb391901381972ba2389fa046
WilliNeu <wang.li.neu@gmail.com>
2020-11-05 08:24:56 +0800

    add homework4

--- sol/hw3/hw3_1.py
+++ sol/hw3/hw3_1.py
@@ -1,10 +1,15 @@
 prompt_text = "Enter a number: "
-user_in = input(prompt_text)
-user_num = int(user_in)
+
+try:
+    user_in = input(prompt_text)
+    user_num = int(user_in)
+except NameError:
+    print("please input a number!")
+
 for i in range(1,10):
- print(i, " times ", user_num, " is ", i*user_num)
-even = (user_num % 2) == 0
-if even:
- print(user_num, " is even")
-else:
- print(user_num, " is odd")
+    print(i, " times ", user_num, " is ", i*user_num)
+    even = (user_num % 2) == 0
+    if even:
+        print(user_num, " is even")
+    else:
+        print(user_num, " is odd")
--- sol/hw3/hw3_2.py
+++ sol/hw3/hw3_2.py
@@ -4,8 +4,8 @@ print("I'm thinking of a number between zero and ten."
  , "Can you guess what it is?")
 guess = 11
 while guess != secret:
-try:
-    guess = int(input("Take a guess: "))
- except ValueError:
-	print("A number! Guess a number!")
+    try:
+        guess = int(input("Take a guess: "))
+    except ValueError:
+        print("A number! Guess a number!")
 print("Well done!")
