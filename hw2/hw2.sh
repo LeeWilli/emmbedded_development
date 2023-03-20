e10c0de10a16c64f342d1c33857fb21a340f1492
wangxuzuo <1572662276@qq.com>
2023-03-20 21:16:46 +0800

    modified hw2

--- hw2/hw2.sh
+++ hw2/hw2.sh
@@ -1,19 +1,20 @@
+#1
 git clone  https://github.com/wangxuzuo/hw2.git
-
+#1.a
 git log --all --graph --decorate
-
+#1.b
 git checkout  README.md
 git log
-
-git blame _config.yml
-git show [commit ID]
-
+#1.c
+git blame hw2.md
+git show 5bca7590
+#2
 echo “test01” >> testStash.txt
 git stash 
 git stash pop
-
+#3 a
 git config --global alias.gitgraph  'git log --all --graph --decorate --oneline'
-
+#3.b
 touch .gitignore_global
 echo “.DS_Store” >>”.gitignore_global”
 git config --global core.excludesfile ~/.gitignore_global
